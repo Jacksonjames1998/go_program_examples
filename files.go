4d2e5997a7e783e1b322203c70184cc9c2da10e2
Jackson James <jacksonkalluvila@gmail.com>
2021-04-05 12:35:57 +0530

    for loop

--- files.go
+++ files.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main(){
-	var i int = 34
+	var i int = 1
 	var j int = 45
 	fmt.Print(j+i)
 	fmt.Printf("\n%v,%T\n",i,i)
+
+	for k :=1;k<=5;k++{
+		fmt.Println("hai jackson",k)
+	}
 }

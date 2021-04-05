01eda991d4da8f915398182dd36e7a36d36f6822
Jackson James <jacksonkalluvila@gmail.com>
2021-04-05 12:49:23 +0530

    function example

--- files.go
+++ files.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+func add(x int, y int) int{
+	var sum = x+y
+	return sum
+}
+
 func main(){
 	var i int = 1
 	var j int = 45
@@ -11,4 +16,9 @@ func main(){
 	for k :=1;k<=5;k++{
 		fmt.Println("hai jackson",k)
 	}
+
+	num1 := 1
+	num2 := 2
+	result := add(num1, num2)
+	fmt.Println(result)
 }
